95eddcf9e0ce410ff39d181a5636c6b8adc95120
Niraj Kheria <nirajkheria@gmail.com>
2022-05-12 23:00:05 +0530

    add graphql type and query

---
+++ Type/ProductType.cs
@@ -0,0 +1,19 @@
+﻿using GraphQL.Types;
+using GraphQLAPI.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GraphQLAPI.Type
+{
+    public class ProductType : ObjectGraphType<Product>
+    {
+        public ProductType()
+        {
+            Field(p => p.Id);
+            Field(p => p.Name);
+            Field(p => p.Price);
+        }
+    }
+}
