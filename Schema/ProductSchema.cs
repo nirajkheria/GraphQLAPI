588573d3b44ee2b8ee7827cf87dd7660bfbb0ae4
Niraj Kheria <nirajkheria@gmail.com>
2022-05-12 23:02:20 +0530

    add query to schema

---
+++ Schema/ProductSchema.cs
@@ -0,0 +1,16 @@
+﻿using GraphQLAPI.Query;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GraphQLAPI.Schema
+{
+    public class ProductSchema : GraphQL.Types.Schema
+    {
+        public ProductSchema(ProductQuery productQuery)
+        {
+            Query = productQuery;
+        }
+    }
+}
