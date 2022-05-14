de688a5b5876c27d498301f0bfe694ea05e20cd1
Niraj Kheria <nirajkheria@gmail.com>
2022-05-14 11:56:09 +0530

    add database

--- Services/ProductService.cs
+++ Services/ProductService.cs
@@ -1,4 +1,5 @@
-﻿using GraphQLAPI.Interfaces;
+﻿using GraphQLAPI.Data;
+using GraphQLAPI.Interfaces;
 using GraphQLAPI.Models;
 using System;
 using System.Collections.Generic;
@@ -9,37 +10,44 @@ namespace GraphQLAPI.Services
 {
     public class ProductService : IProductService
     {
-        private static List<Product> products = new List<Product>
+        private readonly GraphQLDbContext dbContext;
+
+        public ProductService(GraphQLDbContext dbContext)
         {
-            new Product{ Id = 0, Name = "Apple", Price = 10},
-            new Product{ Id = 1, Name = "Mango", Price = 20}
-        };
+            this.dbContext = dbContext;
+        }
 
         public Product AddProduct(Product product)
         {
-            products.Add(product);
+            dbContext.Add(product);
+            dbContext.SaveChanges();
             return product;
         }
 
         public void DeleteProduct(int id)
         {
-            products.RemoveAt(id);
+            var objProduct = dbContext.Products.Find(id);
+            dbContext.Products.Remove(objProduct);
+            dbContext.SaveChanges();
         }
 
         public Product GetProductById(int id)
         {
-            var product = products.Find(p=>p.Id == id);
+            var product = dbContext.Products.Find(id);
             return product;
         }
 
         public List<Product> GetProducts()
         {
-            return products;
+            return dbContext.Products.ToList();
         }
 
         public Product UpdateProduct(int id, Product product)
         {
-            products[id] = product;
+            var objProduct = dbContext.Products.Find(id);
+            objProduct.Name = product.Name;
+            objProduct.Price = product.Price;
+            dbContext.SaveChanges();
             return product;
         }
     }
--- Startup.cs
+++ Startup.cs
@@ -1,6 +1,7 @@
 using GraphiQl;
 using GraphQL.Server;
 using GraphQL.Types;
+using GraphQLAPI.Data;
 using GraphQLAPI.Interfaces;
 using GraphQLAPI.Mutation;
 using GraphQLAPI.Query;
@@ -11,6 +12,7 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -42,18 +44,20 @@ namespace GraphQLAPI
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "GraphQLAPI", Version = "v1" });
             });
             services.AddTransient<IProductService, ProductService>();
-            services.AddSingleton<ProductType>();
-            services.AddSingleton<ProductQuery>();
-            services.AddSingleton<ProductMutation>();
-            services.AddSingleton<ISchema, ProductSchema>();
+            services.AddTransient<ProductType>();
+            services.AddTransient<ProductQuery>();
+            services.AddTransient<ProductMutation>();
+            services.AddTransient<ISchema, ProductSchema>();
 
             services.AddGraphQL(options=> {
                 options.EnableMetrics = false;
             }).AddSystemTextJson();
+
+            services.AddDbContext<GraphQLDbContext>(option => option.UseSqlServer(@"Data Source= (localdb)\MSSQLLocalDB;Initial Catalog=GraphQLDb;Integrated Security = True"));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env, GraphQLDbContext dbContext)
         {
             if (env.IsDevelopment())
             {
@@ -62,6 +66,7 @@ namespace GraphQLAPI
                 //app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "GraphQLAPI v1"));
             }
 
+            dbContext.Database.EnsureCreated();
             app.UseGraphiQl("/graphql");
             app.UseGraphQL<ISchema>();
 
